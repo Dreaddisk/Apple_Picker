fd2515a01b24d2972bd09b44230ab47c8d7bd440
Jose M. Puebla <joey_manuel@outlook.com>
2017-12-11 17:55:00 -0500

    This game is yet not finished.

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    // prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree truns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float SecondsBetweenAppleDrops = 1f;
+
+    void Start() {
+
+        // Dropping apples every second
+        Invoke("DropApple", 2f);    // calls a name function in a certain number of seconds.
+    }
+
+    void DropApple() {  // instantiate an apple at the AppleTree's location.
+
+        GameObject apple = Instantiate<GameObject>(applePrefab);    // Creates an instance of applePrefab and assigns
+                                                                    // it to the GameObject variable apple.
+
+        apple.transform.position = transform.position;              // The position of this new apple GameObject
+                                                                    // is set ot the position of the AppleTree.
+
+        Invoke("DropApple", SecondsBetweenAppleDrops);              // This time Invoke will call the DropApple() function
+                                                                    // in secondsBetweenAppleDrops.
+    }
+
+    void Update() {
+        // Basic Movement
+        Vector3 pos = transform.position;       // This line defines teh vEctor 3 position to be the current position of the apple tree
+        pos.x += speed * Time.deltaTime;        // Makes the movement of the Apple Tree time based.
+        transform.position = pos;               // Assigns this modified pos back to transform.position
+        // Changin Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+
+            speed = Mathf.Abs(speed);   //  Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+
+            speed = -Mathf.Abs(speed);  // Move left
+        }
+        else if (Random.value < chanceToChangeDirections) {
+
+            speed *= -1; // change direction
+        }
+
+    }
+
+    private void FixedUpdate() {
+
+        // Changing Direction Randomlu is now time-based vecause of FixedUpdate()
+        if (Random.value < chanceToChangeDirections) {
+            speed *= -1;    // Change direction
+        }
+    }
+}   // class
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+    void Update() {
+
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // The Camera's z position sets how far to push the mouse into 3D 
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move the x position of this Basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll) {             // any object that collides with this game object, 
+                                                        // OnCollissionEnter will be called.
+
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;      // This line assigns this colliding GameObject 
+                                                        //to the local variable CollidedWith
+
+        if (collidedWith.tag == "Apple") {              // Check to see whether colidedWith is an apple by looking for the 
+                                                        // "Apple" tag that was assinged to all Apple GameObjects.
+
+            Destroy(collidedWith);
+        }
+    }
+}
